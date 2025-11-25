9463128823155188d9e28ca90543541d29de4f28
Miguel Bermudez <miguebermudez19@gmail.com>
2025-11-25 16:03:06 -0600

    Feat: Estructura base del proyecto finalizada y creacion de base de datos

---
+++ database.sql
@@ -0,0 +1,23 @@
+CREATE TABLE usuarios (
+    id_usuarios        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre             VARCHAR(150) NOT NULL,
+    correo             VARCHAR(150) NOT NULL UNIQUE,
+    password         VARCHAR(255) NOT NULL,
+    rol                enum('admin','usuario') DEFAULT 'usuario',
+    estado             TINYINT DEFAULT 1,          -- 1 = activo, 0 = inactivo
+    fecha_creacion     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE tareas (
+    id_tareas          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo             VARCHAR(200) NOT NULL,
+    descripcion        TEXT,
+    estado             enum('finalizada', 'en proceso' , 'pendiente') DEFAULT 'pendiente',  -- pendiente, en_progreso, completada
+    fecha_creacion     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_limite       DATE,
+    id_usuarios        BIGINT NOT NULL,
+
+    CONSTRAINT fk_tareas_usuarios
+        FOREIGN KEY (id_usuarios) REFERENCES usuarios(id_usuarios)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
